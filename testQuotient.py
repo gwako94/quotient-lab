df54d2f2255b6d080da01c2ae04ecace2a58f442
Galgallo Wako <galgallowako254@gmail.com>
2017-10-10 11:17:11 +0300

    Update testQuotient.py

--- testQuotient.py
+++ testQuotient.py
@@ -6,10 +6,10 @@ class TestDivide(unittest.TestCase):
     self.assertRaises(TypeError,Divide, ["string"])
     
   def test_input_is_integer(self):
-	self.assertEqual(Divide(9,3), 3.0)
+    self.assertEqual(Divide(9,3), 3.0)
 	
   def test_input_is_float(self):
-	self.assertEqual(Divide(21.69,3), 7.23)
+    self.assertEqual(Divide(21.69,3), 7.23)
 		
 if __name__ == '__main__':
     unittest.main()
