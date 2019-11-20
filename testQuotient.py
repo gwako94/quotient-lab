bf054699c92d7d96fad121ee934f5e200701894b
Galgallo Wako <galgallowako254@gmail.com>
2019-11-20 15:34:21 +0300

    Update testQuotient.py

--- testQuotient.py
+++ testQuotient.py
@@ -1,11 +1,8 @@
 import unittest
 from Divide import Divide
 
-class TestDivide(unittest.TestCase):
-  def test_TypeError(self):
-    self.assertRaises(TypeError)
-    
-  def test_Divide(self):
+class TestQuotient(unittest.TestCase):
+  def test_Quotient(self):
   	self.assertEqual(Divide(9,3), 3.0)
   	self.assertEqual(Divide(21.69,3), 7.23)
   	
