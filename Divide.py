2641e279d2caf8c0574d99673f2204374a30ce4d
Galgallo Wako <galgallowako254@gmail.com>
2019-11-20 15:31:55 +0300

    Update Divide.py

--- Divide.py
+++ Divide.py
@@ -1,8 +1,9 @@
-def Divide(a,b):
+  # a -> int
+  # b -> int
+  if not isinstance(a, int) or not isinstance(b, int):
+    raise TypeError('Input should be an integer')
   try:
     return a/b
-  except TypeError:
-	return 'Invalid! cannot divide by string'
   except ZeroDivisionError:
-	return 'Invalid! cannot divide by 0'
+	  return 'Invalid! cannot divide by 0'
   
